4421f35ab69cc08414f983beb065410c591aec8f
Danik2023 <danik.kyrteu2@gmail.com>
2025-02-04 15:54:35 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import HomePage from 'pages/HomePage';
+import MovieDetails from 'pages/MovieDetails';
+import Movies from 'pages/Movies';
+import { Routes, Route } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+      </Routes>
     </div>
   );
 };
+
+// const API_KEY = 'a4e0e6c94492c515df52f4a6ebcc54c7';
