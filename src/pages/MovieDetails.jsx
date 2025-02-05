65ac93c8e5d94b57d27c00b5e8c6f56897cd6427
Danik2023 <danik.kyrteu2@gmail.com>
2025-02-05 12:42:16 +0200

    ...

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
 const MovieDetails = () => {
-  return <div>MovieDetails!!!</div>;
+  return (
+    <>
+      <div>MovieDetails!!!</div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
 };
 
 export default MovieDetails;
